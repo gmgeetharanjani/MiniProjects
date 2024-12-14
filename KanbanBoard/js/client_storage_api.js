d78d22c28513d07139637b20c7688e0aada1f39f
Geetha Ranjani GM <63908622+gmgeetharanjani@users.noreply.github.com>
2024-12-14 10:09:43 +0530

    Kanban Boad initital changes

---
+++ KanbanBoard/js/client_storage_api.js
@@ -0,0 +1,12 @@
+/**
+ * Client storage API
+ * 
+ * It is browser API that allows us to store data on user's machine.
+ * Allows to store information such as user preferences, user activity, refresh token, redirection url, cache data without relying on server.
+ * Improves performance and thereby user experience by reducing the number of requests to the server. 
+ * There are majorly three types of client storage apis:
+ * 1. Cookies
+ * 2. Local Storage
+ * 3. Session Storage
+ * 
+ */
